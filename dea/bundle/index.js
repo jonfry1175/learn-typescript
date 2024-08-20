a223f43fc85d576971a6212e55b0dc8dc582f44a
jonfry1175 <odiboen@gmail.com>
2024-08-20 14:38:55 +0700

    tipe data custom tidak harus object

--- dea/bundle/index.js
+++ dea/bundle/index.js
@@ -1,63 +1,7 @@
 "use strict";
-// let myName: string = "Dea";
-// let username : number = 123;
-// let isDead: boolean = false;
-// // any = tipedata bebas
-// let pacarSaya:any;
-// pacarSaya = 5;
-// pacarSaya = "5 bro";
-// pacarSaya = true;
-// // console.log({myName}); dia bakal ngebuat object dengan key myName
-// let pacarLu : number | string  ;
-// pacarLu = 5;
-// pacarLu = "5 bro";
-// // array biasa, bisa pake any
-// const namaMurid: string[] = ["Dea", "Dea1", "Dea2"];
-// console.log({namaMurid});
-// // tupple array = ngikutin tipedata array
-// const namaMurid2: [string, string, string, number] = ["Thomas", "Dea1", "Dea2", 10];
-// console.log({namaMurid2});
-// isDead = true;
-// console.log({myName});
-// console.log({username});
-// console.log({isDead});
-// console.log({pacarSaya});
-// console.log({pacarLu});
-// // tipedata custom
-// // pake "?" jika ingin opsional/tidak diisi
-// type TemanType = {
-//     name: string;
-//     age: number;
-//     isDead: boolean;
-//     isMarried?: boolean;
-// }
-// const teman: TemanType = {
-//     name: "Dea",
-//     age: 10,
-//     isDead: false,
-//     // isMarried: true
-// }
-// console.log({teman});
-// FUNCTION
-// tipedata return
-function create() {
-    return "Dea";
-}
-console.log({ create: create() });
-// pake void jika function tidak return
-const create2 = () => {
-    const a = 1;
-    const b = 2;
-    const c = a + b;
-    console.log("Dea", c);
-};
-create2();
-const sum = (a, b) => {
-    // expected result c = number
-    const c = a + b;
-    console.log("hasil jumlah sum: ", c);
-};
-sum(1, 2);
-const sum2 = (a, b) => `${a} + ${b} = ${a + b}`;
-const resultSum2 = sum2(10, 2);
-console.log({ resultSum2 });
+// // let myName: string = "Dea";
+// // let username : number = 123;
+// // let isDead: boolean = false;
+let sayaManusia;
+sayaManusia = true;
+console.log({ sayaManusia });
--- dea/src/index.ts
+++ dea/src/index.ts
@@ -1,80 +1,93 @@
-// let myName: string = "Dea";
-// let username : number = 123;
-// let isDead: boolean = false;
+// // let myName: string = "Dea";
+// // let username : number = 123;
+// // let isDead: boolean = false;
 
-// // any = tipedata bebas
-// let pacarSaya:any;
-// pacarSaya = 5;
-// pacarSaya = "5 bro";
-// pacarSaya = true;
+// // // any = tipedata bebas
+// // let pacarSaya:any;
+// // pacarSaya = 5;
+// // pacarSaya = "5 bro";
+// // pacarSaya = true;
 
-// // console.log({myName}); dia bakal ngebuat object dengan key myName
+// // // console.log({myName}); dia bakal ngebuat object dengan key myName
 
-// let pacarLu : number | string  ;
-// pacarLu = 5;
-// pacarLu = "5 bro";
+// // let pacarLu : number | string  ;
+// // pacarLu = 5;
+// // pacarLu = "5 bro";
 
-// // array biasa, bisa pake any
-// const namaMurid: string[] = ["Dea", "Dea1", "Dea2"];
-// console.log({namaMurid});
+// // // array biasa, bisa pake any
+// // const namaMurid: string[] = ["Dea", "Dea1", "Dea2"];
+// // console.log({namaMurid});
 
-// // tupple array = ngikutin tipedata array
-// const namaMurid2: [string, string, string, number] = ["Thomas", "Dea1", "Dea2", 10];
+// // // tupple array = ngikutin tipedata array
+// // const namaMurid2: [string, string, string, number] = ["Thomas", "Dea1", "Dea2", 10];
 
-// console.log({namaMurid2});
+// // console.log({namaMurid2});
 
-// isDead = true;
-// console.log({myName});
-// console.log({username});
-// console.log({isDead});
-// console.log({pacarSaya});
-// console.log({pacarLu});
+// // isDead = true;
+// // console.log({myName});
+// // console.log({username});
+// // console.log({isDead});
+// // console.log({pacarSaya});
+// // console.log({pacarLu});
 
-// // tipedata custom
-// // pake "?" jika ingin opsional/tidak diisi
-// type TemanType = {
-//     name: string;
-//     age: number;
-//     isDead: boolean;
-//     isMarried?: boolean;
+// // // tipedata custom
+// // // pake "?" jika ingin opsional/tidak diisi
+// // type TemanType = {
+// //     name: string;
+// //     age: number;
+// //     isDead: boolean;
+// //     isMarried?: boolean;
+// // }
+
+// // const teman: TemanType = {
+// //     name: "Dea",
+// //     age: 10,
+// //     isDead: false,
+// //     // isMarried: true
+// // }
+
+// // console.log({teman});
+
+
+// // FUNCTION
+
+// // tipedata return
+// function create (): string {
+//     return "Dea";
+// }
+// console.log({create:create()});
+
+// // pake void jika function tidak return
+// const create2 = (): void => {
+//      const a = 1;
+//     const b = 2;
+//     const c = a + b;
+//     console.log("Dea", c);
+// };
+// create2();
+
+// const sum = (a: number, b: number): void => {
+//     // expected result c = number
+//     const c: number = a + b;
+//     console.log("hasil jumlah sum: ", c);
 // }
 
-// const teman: TemanType = {
-//     name: "Dea",
-//     age: 10,
-//     isDead: false,
-//     // isMarried: true
-// }
+// sum(1, 2);
 
-// console.log({teman});
+// const sum2 = (a: number, b: number): string => `${a} + ${b} = ${a + b}`;
+
+// const resultSum2 = sum2(10, 2);
+// console.log({resultSum2});
 
 
-// FUNCTION
+// LANJUTAN TIPE DATA CUSTOM
 
-// tipedata return
-function create (): string {
-    return "Dea";
-}
-console.log({create:create()});
+type Wanita = string
+type Pria = boolean
 
-// pake void jika function tidak return
-const create2 = (): void => {
-     const a = 1;
-    const b = 2;
-    const c = a + b;
-    console.log("Dea", c);
-};
-create2();
+type Gender = Wanita | Pria
 
-const sum = (a: number, b: number): void => {
-    // expected result c = number
-    const c: number = a + b;
-    console.log("hasil jumlah sum: ", c);
-}
+let sayaManusia: Gender;
+sayaManusia = true;
 
-sum(1, 2);
-
-const sum2 = (a: number, b: number): string => `${a} + ${b} = ${a + b}`;
-
-const resultSum2 = sum2(10, 2);
-console.log({resultSum2});
+console.log({sayaManusia});
